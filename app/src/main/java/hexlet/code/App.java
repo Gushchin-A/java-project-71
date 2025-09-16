681c96bac0bfdf7649d6fb86b9a6f0b05fcfa98a
Gushchin-A <kpa6ep@gmail.com>
2025-09-16 16:04:05 +0200

    completion of step 3

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,22 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+@Command(
+        name = "gendiff",
+        mixinStandardHelpOptions = true,
+        version = "gendiff 0.1.0",
+        description = "Compares two configuration files and shows a difference."
+)
+
+public class App implements Runnable {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
+    public void run() {
+        System.out.println("Hello-hello!");
     }
 }
